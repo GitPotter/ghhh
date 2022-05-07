7973c8a5990a63cd1353b710a363f660e8a8e0e8
Nethmal Prabashwara <96367042+neth1841@users.noreply.github.com>
2022-05-07 08:03:38 +0530

    Update __main__.py

--- AnyMediaBot/__main__.py
+++ AnyMediaBot/__main__.py
@@ -234,5 +234,26 @@ async def make_logo(_, message):
                               caption=imgcaption,)
     await m.delete() 
 
+
+from pystark import Stark, Message
+from pystark.plugins.helpers import db
+
+
+@bot.on_message(filters.command("broadcast"))
+async def broadcast(bot: Stark, msg: Message):
+    if not msg.reply_to_message and len(msg.command) == 1:
+        await msg.reply("Please reply to a message or pass some text", quote=True)
+        return
+    text = msg.reply_to_message.text if msg.reply_to_message else msg.text[len("/broadcast "):]
+    users = await db.get_all_primary_keys("users")
+    await msg.reply_text("Starting Broadcast...")
+    count = 0
+    for user in users:
+        try:
+            await bot.send_message(user, text)
+            count += 1
+        except Exception as e:
+            Stark.log(f"Exception occurred while sending message to user with id {user}: {e}", "warning")
+    await msg.react(f"Broadcast done successfully to {count} users out of {len(users)}")
     
 bot.run()
